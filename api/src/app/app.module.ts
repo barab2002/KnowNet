5ff4759f440df942c00d51812bddccd41dd17fd7
barab2002 <barab2002@gmail.com>
2026-01-30 17:23:26 +0200

    fix(files): enable serving uploaded profile images via static asset proxy

--- api/src/app/app.module.ts
+++ api/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 import { PostsModule } from '../posts/posts.module';
 import { UsersModule } from '../users/users.module';
 import { AppController } from './app.controller';
@@ -7,6 +9,10 @@ import { AppService } from './app.service';
 
 @Module({
   imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(process.cwd(), 'uploads'),
+      serveRoot: '/uploads',
+    }),
     MongooseModule.forRoot(
       process.env.MONGO_URI || 'mongodb://localhost:27017/knownet',
     ),
