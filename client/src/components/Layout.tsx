65856a115ee2182ed1ee214a47891a0572804061
Idan Tepper <idantepper@gmail.com>
2026-02-24 00:19:55 +0200

    changed logo to be net

--- client/src/components/Layout.tsx
+++ client/src/components/Layout.tsx
@@ -26,7 +26,7 @@ export const Layout = () => {
         <div className="max-w-7xl mx-auto px-4 h-16 flex items-center justify-between gap-4">
           <div className="flex items-center gap-2">
             <div className="w-10 h-10 bg-primary rounded-xl flex items-center justify-center text-white">
-              <span className="material-icons-round">auto_awesome</span>
+              <span className="material-icons-round">hub</span>
             </div>
             <span className="text-xl font-bold tracking-tight">KnowNet</span>
           </div>
