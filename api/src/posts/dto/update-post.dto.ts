7dcbb5ec7e673bf630087a30932322bcca167a45
Idan Tepper <idantepper@gmail.com>
2026-02-24 15:27:25 +0200

    refactor(posts): allow optional content on update dto

--- api/src/posts/dto/update-post.dto.ts
+++ api/src/posts/dto/update-post.dto.ts
@@ -1,12 +1,13 @@
-import { IsNotEmpty, IsString } from 'class-validator';
-import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
+import { ApiPropertyOptional } from '@nestjs/swagger';
 
 export class UpdatePostDto {
-  @ApiProperty({
+  @ApiPropertyOptional({
     description: 'Updated text content of the post',
     example: 'Updated post content with new insights.',
   })
   @IsString()
+  @IsOptional()
   @IsNotEmpty()
-  content: string;
+  content?: string;
 }
